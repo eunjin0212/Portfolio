8cd8cb05a8a160d3d974112743ade40361f06601
eunjin0212 <eunjin9639@gmail.com>
2021-03-28 15:46:57 +0900

    Modified Hero Data

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,7 +11,7 @@ export const headData = {
 export const heroData = {
   title: 'Hi, my name is',
   name: 'Eunjin Kim',
-  subtitle: "I'm the Front-end Developer.",
+  subtitle: "I'm a Front-end Developer.",
   cta: '',
 };
 
